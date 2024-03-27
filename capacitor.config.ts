908d079a652123ea23ed5b8b38462dfc929bac1a
Neilfe Amor <solanoneilfe@gmail.com>
2024-03-27 13:34:09 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ACTIVITY-5-SOLANO',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
